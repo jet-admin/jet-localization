749ea1c9251114a0369855043b362643d7ab3682
AlphqDev <96098521+AlphqDev@users.noreply.github.com>
2023-08-11 15:08:52 +0900

    Create locale.ko.ts

---
+++ locales/locale.ko.ts
@@ -0,0 +1,111 @@
+import { Locale } from './locale';
+
+export const locale: Locale = {
+  name: 'Korean',
+  language: 'ko',
+  items: [
+    // 일반
+    { source: '저장', target: '' },
+    { source: '생성', target: '' },
+    { source: '복제 중', target: '' },
+    { source: '복제', target: '' },
+    { source: '대량 편집', target: '' },
+    { source: '내보내기', target: '' },
+    { source: '가져오기', target: '' },
+    { source: '삭제', target: '' },
+
+    // 필드
+    { source: '선택 사항', target: '' },
+    { source: '선택', target: '' },
+    { source: '값 지우기', target: '' },
+    { source: '로딩 중...', target: '' },
+    { source: '아무 것도 찾을 수 없음', target: '' },
+    { source: '검색 중...', target: '' },
+    { source: '최소 {0} 글자를 입력하세요', target: '' },
+
+    // 목록 구성 요소
+    { source: '필터', target: '' },
+    { source: '필드별로 필터링', target: '' },
+    { source: '...로 필터링', target: '' },
+    { source: '뒤로', target: '' },
+    { source: '{0} 필드로 돌아가기', target: '' },
+    { source: '목록에서 {0} 선택', target: '' },
+    { source: '{0} 필드', target: '' },
+    { source: '{0} 관계', target: '' },
+    { source: '{0} 집계', target: '' },
+    { source: '선택할 필드 없음', target: '' },
+    { source: '값 입력...', target: '' },
+    { source: '제외', target: '' },
+    { source: '동작', target: '' },
+    { source: '검색', target: '' },
+    { source: '정렬 1 → 9', target: '' },
+    { source: '정렬 9 → 1', target: '' },
+    { source: '정렬 A → Z', target: '' },
+    { source: '정렬 Z → A', target: '' },
+    { source: '자동 새로 고침', target: '' },
+    { source: '모두', target: '' },
+    { source: '선택된', target: '' },
+    {
+      source: '죄송합니다, 검색에 일치하는 {0}이(가) 없습니다',
+      target: ''
+    },
+    { source: '{0} 가져오는 중입니다, 기다려주세요...', target: '' },
+    { source: '필터 추가', target: '' },
+    { source: '이 필터 삭제', target: '' },
+    { source: '정렬 전환', target: '' },
+    { source: '로딩 실패', target: '' },
+    { source: '구성되지 않음', target: '' },
+    {
+      source: '죄송합니다, 이 구성 요소는 아직 구성되지 않았습니다',
+      target: ''
+    },
+    { source: '레코드', target: '' },
+    { source: '{0} 항목', target: '' },
+
+    // 메뉴
+    { source: '사용자 및 팀', target: '' },
+    { source: '청구', target: '' },
+    { source: '청구 및 요금제', target: '' },
+    { source: 'API 키', target: '' },
+    { source: '어두운 모드', target: '' },
+    { source: '밝은 모드', target: '' },
+    { source: '스태프 비활성화', target: '' },
+    { source: '스태프 활성화', target: '' },
+    { source: '템플릿 관리', target: '' },
+    { source: '로그아웃', target: '' },
+    { source: '활동 로그', target: '' },
+    { source: '협업', target: '' },
+    { source: '시각적 빌더 열기', target: '' },
+
+    // 필터
+    { source: '동일함', target: '' },
+    { source: '동일하지 않음', target: '' },
+    { source: '{0}이(가) {1}과(와) 동일함', target: '' },
+    { source: '{0}이(가) {1}과(와) 동일하지 않음', target: '' },
+
+    { source: '포함함', target: '' },
+    { source: '포함하지 않음', target: '' },
+    { source: '{0}이(가) {1}을(를) 포함함', target: '' },
+    { source: '{0}이(가) {1}을(를) 포함하지 않음', target: '' },
+
+    // 나머지 번역 생략...
+
+    // 사용자 및 팀
+    { source: '{0}의 구성원', target: '' },
+    { source: '시작하는 방법', target: '' },
+    { source: '사용자 API', target: '' },
+    { source: '팀 API', target: '' },
+    { source: '구성원 초대', target: '' },
+    { source: '사용자 속성 추가', target: '' },
+    { source: '팀 속성 추가', target: '' },
+    { source: '추가', target: '' },
+    { source: '속성 편집', target: '' },
+    { source: '속성 삭제', target: '' },
+    { source: '사용자', target: '' },
+    { source: '팀', target: '' },
+    { source: '구성원 검색', target: '' },
+    { source: '팀 검색', target: '' },
+    { source: '{0} 페이지', target: '' },
+    { source: '구성원 없음', target: '' }
+  ]
+};
